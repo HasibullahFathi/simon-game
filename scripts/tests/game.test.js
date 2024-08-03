884dd500bb5cc03f895e525eccbd5e8e4689d561
HasibullahFathi <hasibullah.fathi@gmail.com>
2024-08-03 09:05:17 +0000

    the choices should contain the ids of the buttons

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -17,15 +17,19 @@ describe("game object contains correct keys", () => {
             expect("score" in game).toBe(true);
         });
 
-        test("current game key exists", () => {
+        test("currentGame key exists", () => {
             expect("currentGame" in game).toBe(true);
         });
 
-        test("player moves key exists", () => {
+        test("playerMoves key exists", () => {
             expect("playerMoves" in game).toBe(true);
         });
 
         test("choices key exists", () => {
             expect("choices" in game).toBe(true);
+        });
+
+        test("choices should contain correct ids", () => {
+            expect(game.choices).toEqual(["button1, button2, button3, button4"]);
         });
 });
