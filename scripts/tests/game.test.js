fc1497238b01cf42d7f9c2177c21d61098f66399
HasibullahFathi <hasibullah.fathi@gmail.com>
2024-08-03 08:45:20 +0000

    write first test to check if score key exists

---
+++ scripts/tests/game.test.js
@@ -0,0 +1,17 @@
+/**
+ * @jest-environment jsdom
+ */
+
+beforeAll(() => {
+    let fs = require('fs');
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+});
+
+describe("Game object contains correct keys", () => {
+    test("Score key exists", () => {
+            expect("score" in game).toBe(true);
+        });
+});
